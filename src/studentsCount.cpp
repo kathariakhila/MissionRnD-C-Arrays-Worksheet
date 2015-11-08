18bd19bb8f3438117777352b447e3dbd7237e6da
kathariakhila <kathariakhila@gmail.com>
2015-11-08 23:04:29 +0530

    Submit Commit

--- src/studentsCount.cpp
+++ src/studentsCount.cpp
@@ -15,6 +15,125 @@ NOTES:
 
 #include <stdio.h>
 
+
+
+int ascPosition(int *Arr, int len, int score) {
+	int lowerBound = 0;
+	int upperBound = len - 1;
+
+	while (true) {
+		int mid = (upperBound + lowerBound) / 2;
+		if (len == 0) {
+			return mid = 0;
+		}
+		if (lowerBound == mid) {
+			if (Arr[mid] > score) {
+				return mid;
+			}
+		}
+		if (Arr[mid] < score) {
+			lowerBound = mid + 1;          // its in the upper 
+			if (lowerBound > upperBound) {
+				return mid += 1;
+			}
+		}
+		else if (lowerBound > upperBound) {
+			return mid;
+		}
+		else {
+			upperBound = mid - 1;          // its in the lower   
+		}
+	}
+}
+
+int decPosition(int *Arr, int len, int score) {
+	int lowerBound = 0;
+	int upperBound = len - 1;
+
+	while (true) {
+		int mid = (upperBound + lowerBound) / 2;
+		if (len == 0) {
+			return mid = 0;
+		}
+		if (lowerBound == mid) {
+			if (Arr[mid] < score) {
+				return mid;
+			}
+		}
+		if (Arr[mid] > score) {
+			lowerBound = mid + 1;          // its in the upper 
+			if (lowerBound > upperBound) {
+				return mid += 1;
+			}
+		}
+		else if (lowerBound > upperBound) {
+			return mid;
+		}
+		else {
+			upperBound = mid - 1;          // its in the lower   
+		}
+	}
+}
+
+
 void * studentsCount(int *Arr, int len, int score, int *lessCount, int *moreCount) {
-	return NULL;
+
+	int p;
+
+	if (Arr == NULL)
+		return NULL;
+	
+	if (len < 0)
+		return NULL;
+
+	else if (len == 1){
+		if (Arr[0] < score){
+			*lessCount = 1;
+			*moreCount = 0;
+		}
+		
+		else if (Arr[0] > score){
+			*lessCount = 0;
+			*moreCount = 1;
+		}
+		else{
+			*lessCount = 0;
+			*moreCount = 0;
+		}
+	}
+
+	else{
+		if (Arr[0] < Arr[len-1]){
+			p = ascPosition(Arr, len, score);
+
+			*lessCount = p;
+			if (Arr[p] == score)
+				*moreCount = len - p - 1;
+			else
+				*moreCount = len - p;
+		}
+		else if (Arr[0] > Arr[len-1]){
+			p = decPosition(Arr, len, score);
+
+			*lessCount = p;
+			if (Arr[p] == score)
+				*moreCount = len - p - 1;
+			else
+				*moreCount = len - p;
+		}
+		else {
+			if (Arr[0] < score){
+				*lessCount = len;
+				*moreCount = 0;
+			}
+			else if (Arr[0] > score){
+				*moreCount = len;
+				*lessCount = 0;
+			}
+			else
+				*lessCount = *moreCount = 0;
+
+		}
+	
+	}
 }
