a03c4609bd31bb31e1696496504b44a3337c117b
kathariakhila <kathariakhila@gmail.com>
2015-11-09 23:54:19 +0530

    Submit Commit

--- src/sortedArrayPositionsChange.cpp
+++ src/sortedArrayPositionsChange.cpp
@@ -13,7 +13,58 @@ NOTES:
 
 #include <stdio.h>
 
+
+void swap(int *a, int number1, int number2){
+
+	int temp;
+	temp = a[number1];
+	a[number1] = a[number2];
+	a[number2] = temp;
+
+	return;
+}
+
+
 void * sortedArrayPositionsChange(int *Arr, int len)
+
 {
-	return NULL;
+	int i = 0;
+	int index;
+
+
+	if (Arr == NULL)
+		return NULL;
+
+	if (len < 0)
+		return NULL;
+
+	else{
+
+		while (Arr[i] < Arr[i + 1] && i < len){
+
+			i++;
+
+		}
+
+		if (i == len){
+
+		}
+
+		
+
+		else{
+
+			index = i;
+			i = len - 1;
+
+			while (Arr[i]  > Arr[index])
+				i--;
+
+
+			swap(Arr, index, i);
+		}
+	
+
+	}
+
 }
