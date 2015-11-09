784ff9e3f4718e76acc1be5cb3d78063ecf85149
kathariakhila <kathariakhila@gmail.com>
2015-11-09 18:09:29 +0530

    Submit Commit

--- src/sortedArrayInsertNumber.cpp
+++ src/sortedArrayInsertNumber.cpp
@@ -14,7 +14,68 @@ NOTES: Use realloc to allocate memory.
 #include <stdio.h>
 #include <malloc.h>
 
-int * sortedArrayInsertNumber(int *Arr, int len, int num)
-{
-	return NULL;
+
+int position(int *Arr, int len, int num) {
+	int lowerBound = 0;
+	int upperBound = len - 1;
+
+	while (true) {
+		int mid = (upperBound + lowerBound) / 2;
+		if (len == 0) {
+			return mid = 0;
+		}
+		if (lowerBound == mid) {
+			if (Arr[mid] > num) {
+				return mid;
+			}
+		}
+		if (Arr[mid] < num) {
+			lowerBound = mid + 1;           
+			if (lowerBound > upperBound) {
+				return mid += 1;
+			}
+		}
+		else if (lowerBound > upperBound) {
+			return mid;
+		}
+		else {
+			upperBound = mid - 1;             
+	
+		}
+	}
+}
+
+
+void insertPosition(int *Arr, int len, int num, int p){
+
+	int i = len;
+
+	for (i = len; i > p; i--){
+		Arr[i] = Arr[i - 1];
+
+	}
+	Arr[p] = num;
+
+
+}
+
+
+
+int * sortedArrayInsertNumber(int *Arr, int len, int num){
+
+
+	int p;
+
+	if (Arr == NULL )
+		return NULL;
+
+	if (len < 0)
+		return NULL;
+
+	else{
+
+		p = position(Arr, len, num);
+		insertPosition(Arr, len, num, p);
+		return Arr;
+	}
 }
