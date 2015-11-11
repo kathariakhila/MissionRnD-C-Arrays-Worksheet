eab6c0fdfbbd371202d80a7b415a0a22fc9a6dbc
kathariakhila <kathariakhila@gmail.com>
2015-11-11 13:30:59 +0530

    Submit Commit

--- src/topKStudents.cpp
+++ src/topKStudents.cpp
@@ -21,6 +21,93 @@ struct student {
 	int score;
 };
 
+
+void highest(struct student *students, int len, int k, struct student *res){
+
+	struct student max;
+
+	max.score = 0;
+
+	int i;
+	int j;
+
+	for (i = 0; i<len; i++){
+
+		if (students[i].score > max.score){
+			max.name = students[i].name;
+			max.score = students[i].score;
+		}
+
+		res[k-1].name = max.name;
+		res[k-1].score = max.score;
+	}
+
+	for (i = k-2; i>=0; i--){
+
+		max.score = 0;
+
+		for (j = 0; j<len; j++){
+
+			if (students[j].score > max.score && students[j].score < res[i + 1].score){
+				max.name = students[j].name;
+				max.score = students[j].score;
+
+			}
+		}
+
+		res[i].name = max.name;
+		res[i].score = max.score;
+	}
+
+}
+
+
 struct student ** topKStudents(struct student *students, int len, int K) {
-	return NULL;
+	
+	struct student res[20] ;
+
+	struct student **topK = (struct student**)calloc(K, sizeof(struct student));
+	int i,j;
+	int index;
+
+
+	if (students == NULL)
+		return NULL;
+	
+	if (len < 0)
+		return NULL;
+
+	if (K <= 0)
+		return NULL;
+	
+	else if (K > len){
+
+		highest(students, len, len, res);
+		for (i = 0; i < K; i++){
+			students[i].name = res[i].name;
+			students[i].score = res[i].score;
+			//topK[i] = &res[i];
+		}
+		for (i = 0; i < K; i++)
+			topK[i] = &students[i];
+
+		return topK;
+	}
+	else{
+		highest(students, len, K, res);
+		
+
+		for (i = 0; i < K; i++){
+			students[i].name = res[i].name;
+			students[i].score = res[i].score;
+			//topK[i] = &res[i];
+		}
+		for (i = 0; i < K; i++)
+			topK[i] = &students[i];
+
+		return topK;
+	}
+
+
+
 }
